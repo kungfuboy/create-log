596bb694c152a302359ff68faa71e010c647efbd
夜鹰 <17kungfuboy@gmail.com>
2021-09-02 18:52:01 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd dist/
 git init .
 git add .
 git commit -m'deploy'
-git push -f https://github.com/kungfuboy/jp-color-card.git master:gh-pages
+git push -f https://github.com/kungfuboy/create-log.git master:gh-pages
 cd -
