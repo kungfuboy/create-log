d0a5a360f7b9c5a9bf9f3c52f23391d0130b7fbe
夜鹰 <17kungfuboy@gmail.com>
2021-09-02 18:53:51 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd dist/
 git init .
 git add .
 git commit -m'deploy'
-git push -f https://github.com/kungfuboy/create-log.git master:gh-pages
+git push -f git@github.com:kungfuboy/create-log.git master:gh-pages
 cd -
